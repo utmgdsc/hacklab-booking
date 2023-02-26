3dde6f79eaed41edbfdb3b51a0b94d8f920a2079
logonoff <logonoff@users.noreply.github.com>
2023-02-26 15:26:03 -0500

    HB-2 add no name edgecase in `InitialsAvatar`

--- frontend/src/components/InitialsAvatar/InitialsAvatar.jsx
+++ frontend/src/components/InitialsAvatar/InitialsAvatar.jsx
@@ -2,31 +2,33 @@ import { Avatar } from "@mui/material";
 
 /**
  * get a hash of a string that is also a color code
+ * @param {string} name the name to get the hash of
+ * @param {number} lightness the lightness of the color
+ * @returns {string} the color code in hsl format
  */
-const colorHash = ({name}) => {
-    let hash = 0;
+const colorHash = ({ name = "", lightness = 50 }) => {
+    let hash = 95;
+
+    // use djb2 algorithm to get a hash of the name
     for (let i = 0; i < name.length; i++) {
+        // ascii + hash * 33
         hash = name.charCodeAt(i) + ((hash << 5) - hash);
     }
-    let color = '#';
-    for (let i = 0; i < 3; i++) {
-        let value = (hash >> (i * 8)) & 0xFF;
-        color += ('00' + value.toString(16)).substr(-2);
-    }
-    return color;
+
+    const hue = Math.abs(hash % 120);
+    const saturation = Math.abs(hash % 100);
+
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 };
 
 /**
  * Given a name get the initials of the name up to 2 characters into an avatar
  * @param {string} name the name to get the initials of
  */
-export const InitialsAvatar = ({ name }) => {
+export const InitialsAvatar = ({ name = "" }) => {
     return (
-        <Avatar alt={name} style={{
-            backgroundColor: colorHash({name}),
-        }}>
-            { name.split(" ").map((word) => word[0]).slice(0, 2).join("").toUpperCase() }
+        <Avatar alt={name} style={{ backgroundColor: colorHash({ name }) }}>
+            { name.match(/(^\S\S?|\s\S)?/g).map(v => v.trim()).join("").match(/(^\S|\S$)?/g).join("").toLocaleUpperCase()}
         </Avatar>
-
     );
 };
