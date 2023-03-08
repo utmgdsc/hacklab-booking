d3f1284397badfe3f1ecd9edaea6010aa9b14e6e
logonoff <logonoff@users.noreply.github.com>
2023-03-07 22:48:08 -0500

    HB-4 update booking
    Co-Authored-By: Daniyal Bokhari <63890998+daniyalb@users.noreply.github.com>

--- frontend/src/pages/CreateBooking/index.jsx
+++ frontend/src/pages/CreateBooking/index.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import { React, useState } from "react";
 import {
     Button,
     TextField,
-    Box
+    Box,
+    Stepper,
+    StepLabel,
+    Step
 } from "@mui/material";
+
 import { SubPage } from "../../layouts/SubPage";
 import SearchIcon from '@mui/icons-material/Search';
 
 export const CreateBooking = () => {
+    const steps = ['Reason', 'Details', 'Date & Time'];
+    const [activeStep, setActiveStep] = useState(0);
+    const [isComplete, setIsComplete] = useState(false);
+
+    const handleNext = () => {
+        setActiveStep(activeStep+1);
+    }
+
+    const handleBack = () => {
+        setActiveStep(activeStep-1);
+    }
+
     return (
         <SubPage name="Create a booking">
             <Box sx={{
@@ -18,7 +34,44 @@ export const CreateBooking = () => {
                 marginBottom: "4em"
             }}
             >
+                <Stepper activeStep={activeStep}>
+                    {steps.map((step) => (
+                        <Step>
+                            <StepLabel>{step}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+            </Box>
 
+            <Box sx={{
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+                flexWrap: "nowrap",
+                marginBottom: "4em"
+            }}
+            >
+                null
+            </Box>
+
+            <Box sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between"
+            }}>
+                <Button
+                onClick={handleBack}
+                variant="contained"
+                disabled={activeStep === 0}
+                >
+                    Back
+                </Button>
+                <Button
+                onClick={handleNext}
+                variant="contained"
+                >
+                    Next
+                </Button>
             </Box>
         </SubPage>
     );
