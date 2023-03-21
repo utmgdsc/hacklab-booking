a58e1db815776cc3b65eee9afea7f61c9a7c87b5
logonoff <logonoff@users.noreply.github.com>
2023-03-21 02:42:43 -0400

    HB-2 remove tracking page

--- frontend/src/pages/Track/index.jsx
+++ frontend/src/pages/Track/index.jsx
@@ -14,58 +14,8 @@ export const Track = () => {
     const navigate = useNavigate();
 
     return (
-        <SubPage name="Track a request">
-            <Box sx={{
-                display: "flex",
-                justifyContent: "space-around",
-                alignItems: "center",
-                flexWrap: "nowrap",
-                marginBottom: "4em",
-                component: "form"
-            }}
-            >
-                <TextField
-                    fullWidth
-                    label="Tracking ID"
-                    id="fullWidth"
-                    defaultValue={id}
-                    onKeyPress={(ev) => {
-                        if (ev.key === 'Enter') {
-                            // press enter to navigate to track page
-                            navigate("/track/" + document.getElementById("fullWidth").value);
-                            ev.preventDefault();
-                        }
-                    }}
-                />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    id="track-button"
-                    onClick={() => {
-                        navigate("/track/" + document.getElementById("fullWidth").value);
-                    }}
-                    sx={{
-                        marginLeft: "1em",
-                        padding: "0 1.5em",
-                        height: "56px"
-                    }}>
-                    <SearchIcon />
-                    Track
-                </Button>
-            </Box>
-            <Box>
-                {/* <h1>Tracking ID: {id}</h1> */}
+        <SubPage name="All requests">
 
-                {
-                    (!isNaN(parseFloat(id))) &&
-                    <ActiveRequestCard
-                        title="Machine Learning Workshop"
-                        date="2021-10-10"
-                        location="DH 2014 (Hacklab)"
-                        status={id}
-                    />
-                }
-            </Box>
         </SubPage>
     );
 };
