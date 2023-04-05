9a298377064ef25f7aacf8105973a4e034e2adc4
Daksh <daksh3451@gmail.com>
2023-04-05 15:28:36 -0400

    create event

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Account } = require("../models/accounts");
 const { roleVerify } = require("../middleware/role_middleware");
+const {addEvent} = require("../google/test");
 
 router.get("/info", async (req, res) => {
   console.log(req.headers);
@@ -36,15 +37,30 @@ router.post('/modifyAccess/:id', roleVerify(['admin']), async (req, res) => {
     let account = await Account.findOne({ utorid: req.params.id });
     account.accessGranted = req.body.accessGranted;
     account.save();
+
     res.send(account);
 
     // for all requests in which this account is an owner, if the current status is "approval," then change it to "complete"
-    let requests = await Request.find({owner: account._id});
+    let requests = await Request.find({owner: account["_id"]});
 
     if (account.accessGranted) {
       for (let i = 0; i < requests.length; i++) {
           if (requests[i].status === "approval") {
               requests[i].status = "completed";
+              const event = {
+                'summary': `HB ${request["_id"]} ${request.title}`,
+                'location': 'DH2014',
+                'description': `${request.description} ${request.reason}`,
+                'start': {
+                  'dateTime': `${request.start_date.toISOString()}`,
+                  'timeZone': 'America/Toronto',
+                },
+                'end': {
+                  'dateTime': `${request.end_date.toISOString()}`,
+                  'timeZone': 'America/Toronto',
+                }
+              };
+              await addEvent(event);
               await requests[i].save();
           }
       }
