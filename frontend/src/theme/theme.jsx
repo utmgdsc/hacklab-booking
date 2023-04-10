3c0ddaf8581360bb8236601895c32bc63b0c47ff
logonoff <logonoff@users.noreply.github.com>
2023-04-09 21:10:58 -0400

    theme changing works now

--- frontend/src/theme/theme.jsx
+++ frontend/src/theme/theme.jsx
@@ -1,6 +1,5 @@
 import { createTheme } from '@mui/material/styles';
 import { grey } from '@mui/material/colors';
-import React from 'react';
 
 /**
  * enum for theme types
@@ -19,103 +18,106 @@ export const THEME = {
     /** System default theme
      * @type {string}
      */
-    DEFAULT: "default",
+    DEFAULT: "system",
 }
 
-const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-
-// const getTheme = localStorage.getItem("theme") === "system" ? systemTheme() : localStorage.getItem("theme") === "dark" ? "dark" : "light";
-
-export const GoogleTheme = createTheme({
-    palette: {
-        mode: THEME.LIGHT,
-        primary: {
-            main: "#4285f4",
-        },
-        error: {
-            main: "#ea4335",
-        },
-        warning: {
-            main: "#fbbc04",
-        },
-        info: {
-            main: "#4285f4",
-        },
-        success: {
-            main: "#0f9d58",
-        },
-        action: {
-            light: {
-                active: "#000",
-                disabled: "#5f6368",
-            }
-        },
-        gray: {
-            main: "#5f6368",
-        },
-        // Used by `getContrastText()` to maximize the contrast between
-        // the background and the text.
-        contrastThreshold: 3,
-        // Used by the functions below to shift a color's luminance by approximately
-        // two indexes within its tonal palette.
-        // E.g., shift from Red 500 to Red 300 or Red 700.
-        tonalOffset: 0.2,
-    },
-    typography: {
-        fontFamily: [
-            "-apple-system",
-            "BlinkMacSystemFont",
-            "Segoe UI",
-            "Roboto",
-            "Helvetica Neue",
-            "Arial",
-            "sans-serif"
-        ].join(","),
-        h1: {
-            fontSize: "2.5em",
-            fontWeight: 500,
-            lineHeight: 1.2,
-            letterSpacing: "-0.00833em",
-        },
-        h2: {
-            fontSize: "2em",
-            fontWeight: 500,
-            lineHeight: 1.2,
-            letterSpacing: "-0.00833em",
-        },
-        h3: {
-            fontSize: "1.75em",
-            fontWeight: 500,
-            lineHeight: 1.2,
-            letterSpacing: "-0.00833em",
-        },
-        h4: {
-            fontSize: "1.5em",
-            fontWeight: 500,
-            lineHeight: 1.2,
-            letterSpacing: "-0.00833em",
-        },
-        h5: {
-            fontSize: "1.25em",
-            lineHeight: 1.2,
-            letterSpacing: "-0.00833em",
-        },
-        gray: {
-            color: grey[600]
-        }
-    },
-    components: {
-        MuiCard: {
-            styleOverrides: {
-                root: {
-                    width: "100%",
-                    margin: "1em 0",
-                    borderRadius: "1em",
-                    border: "1px solid #e8eaed",
-                    boxShadow: "unset"
+export const GoogleTheme = ({ mode }) => {
+    return ({
+        palette: {
+            mode: mode,
+            primary: {
+                main: "#4285f4",
+            },
+            error: {
+                main: "#ea4335",
+            },
+            warning: {
+                main: "#fbbc04",
+            },
+            info: {
+                main: "#4285f4",
+            },
+            success: {
+                main: "#0f9d58",
+            },
+            action: {
+                light: {
+                    active: "#000",
+                    disabled: "#5f6368",
                 }
             },
-
+            gray: {
+                light: {
+                    main: "#5f6368"
+                },
+                dark: {
+                    main: "#202124"
+                }
+            },
+            // Used by `getContrastText()` to maximize the contrast between
+            // the background and the text.
+            contrastThreshold: 3,
+            // Used by the functions below to shift a color's luminance by approximately
+            // two indexes within its tonal palette.
+            // E.g., shift from Red 500 to Red 300 or Red 700.
+            tonalOffset: 0.2,
+        },
+        typography: {
+            fontFamily: [
+                "-apple-system",
+                "BlinkMacSystemFont",
+                "Segoe UI",
+                "Roboto",
+                "Helvetica Neue",
+                "Arial",
+                "sans-serif"
+            ].join(","),
+            h1: {
+                fontSize: "2.5em",
+                fontWeight: 500,
+                lineHeight: 1.2,
+                letterSpacing: "-0.00833em",
+            },
+            h2: {
+                fontSize: "2em",
+                fontWeight: 500,
+                lineHeight: 1.2,
+                letterSpacing: "-0.00833em",
+            },
+            h3: {
+                fontSize: "1.75em",
+                fontWeight: 500,
+                lineHeight: 1.2,
+                letterSpacing: "-0.00833em",
+            },
+            h4: {
+                fontSize: "1.5em",
+                fontWeight: 500,
+                lineHeight: 1.2,
+                letterSpacing: "-0.00833em",
+            },
+            h5: {
+                fontSize: "1.25em",
+                lineHeight: 1.2,
+                letterSpacing: "-0.00833em",
+            },
+            gray: {
+                color: grey[600]
+            }
+        },
+        components: {
+            MuiCard: {
+                styleOverrides: {
+                    root: {
+                        width: "100%",
+                        margin: "1em 0",
+                        borderRadius: "1em",
+                        boxShadow: "unset",
+                        border: "1px solid",
+                        borderColor: mode === THEME.LIGHT ? "#e8eaed" : "#0a0a0a",
+                    }
+                }
+            },
         },
         MuiCardContent: {
             styleOverrides: {
@@ -135,4 +137,5 @@ export const GoogleTheme = createTheme({
             }
         }
     }
-});
+    )
+}
