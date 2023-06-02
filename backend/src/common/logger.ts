fa0c3abe8a845152c9a393c743afc5c18e39ca58
Ido <benhaim.ido@gmail.com>
2023-06-02 02:02:04 -0400

    Backend docker finalized (for now)

--- backend/src/common/logger.ts
+++ backend/src/common/logger.ts
@@ -12,21 +12,21 @@ const logLevels = {
 
 const logger :Logger = createLogger({
   levels: logLevels,
-  format: format.json(),
+  format: format.combine(format.timestamp(), format.json()),
   transports: [
     //
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
-    new transports.Console({ level: 'info' }),
-    // new transports.File({ filename: './logs/error.log', format: format.combine(format.timestamp(), format.json()), level: 'error' }),
-    // new transports.File({ filename: './logs/combined.log', format: format.combine(format.timestamp(), format.json()) }),
+    new transports.File({ filename: './logs/error.log', level: 'error' }),
+    new transports.File({ filename: './logs/combined.log' }),
   ],
 });
 
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new transports.Console({
     format: format.combine(
+      format.timestamp(),
       format.splat(),
       format.simple(),
     ),
