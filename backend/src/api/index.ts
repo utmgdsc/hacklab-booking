27107a17736fa125bd5437e0d483f6e046cf17b3
Ido <benhaim.ido@gmail.com>
2023-06-07 17:53:49 -0400

    Added auth and error handling middleware

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -2,14 +2,53 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes';
 import logger from '../common/logger';
+import { sendResponse } from './utils';
+import db from '../common/db';
 
 const port = process.env.PORT || 3000;
 const app = express();
-
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`);
+  try {
+    next();
+  } catch (e) {
+    logger.error(e);
+    let message = 'Internal server error.';
+    if (process.env.NODE_ENV === 'development') {
+      message += `\n${e}`;
+    }
+    sendResponse(res, {
+      status: 500,
+      message,
+    });
+  }
+});
 app.use(bodyParser.json());
+app.use(async (req, res, next) => {
+  if (!req.headers.utorid || !req.headers.http_mail || !req.headers.http_cn) {
+    sendResponse(res, {
+      status: 401,
+      message: 'Missing Shibboleth headers.',
+    });
+    return;
+  }
+  req.user = await db.user.upsert({
+    where: { utorid: req.header('utorid') },
+    update: { email: req.header('http_mail') as string },
+    create: {
+      utorid: req.header('utorid') as string,
+      email: req.header('http_mail') as string,
+      name: req.header('http_cn') as string,
+    },
+  });
+  next();
+
+});
+
 
 app.use(routes);
 
 app.listen(port, () => {
-  logger.info(`Server is listening on port ${  port  }.`);
+  logger.info(`Server is listening on port ${port}.`);
 });
+
--- backend/src/api/utils/index.ts
+++ backend/src/api/utils/index.ts
@@ -4,8 +4,24 @@ import {
   RequestHandler,
   NextFunction,
 } from 'express';
+import ModelResponse, {
+  ModelResponseError,
+  ModelResponseSuccess,
+} from '../../types/ModelResponse';
 
 export const routeNotImplemented: RequestHandler = (req: Request, res: Response, next: NextFunction | undefined) => {
   res.statusCode = 501;
   res.json({ message: 'Not implemented' });
+};
+
+
+export const sendResponse = <T>(res: Response, model: ModelResponse<T>) => {
+  res.statusCode = model.status;
+  if ((<ModelResponseSuccess<T>>model).data) {
+    res.json((model as ModelResponseSuccess<T>).data);
+    return;
+  } else {
+    res.json(model);
+    return;
+  }
 };
