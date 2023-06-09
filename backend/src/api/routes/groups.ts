9af388c14ffaf8caf78f6dbf79863a80918f0112
Ido <benhaim.ido@gmail.com>
2023-06-09 16:55:38 -0400

    Finished groups api

--- backend/src/api/routes/groups.ts
+++ backend/src/api/routes/groups.ts
@@ -1,15 +1,39 @@
 import { Router } from 'express';
-import { routeNotImplemented } from '../utils';
+import { checkRequiredFields, routeNotImplemented, sendResponse } from '../utils';
+import groupsModel from '../../models/groupsModel';
 
 const router = Router();
 
-router.get('/', routeNotImplemented);
-router.get('/:id', routeNotImplemented);
-router.post('/create', routeNotImplemented);
-router.post('/:id/changerole', routeNotImplemented);
-router.post('/:id/invite', routeNotImplemented);
-router.post('/:id/invite/accept', routeNotImplemented);
-router.post('/:id/invite/reject', routeNotImplemented);
+router.get('/', async (req, res) => {
+  sendResponse(res, await groupsModel.getGroups(req.user));
+});
+router.post('/create', checkRequiredFields(['name']), async (req, res) => {
+  sendResponse(res, await groupsModel.createGroup(req.body.name, req.user));
+});
+router.use('/:id', (req, res, next) => {
+  const id = parseInt(req.params.id);
+  if (isNaN(id) || !isFinite(id)) {
+    res.statusCode = 400;
+    res.json({ message: 'Invalid group ID.' });
+    return;
+  }
+  next();
+});
+router.get('/:id',  async (req, res) => {
+  sendResponse(res, await groupsModel.getGroup(parseInt(req.params.id), req.user));
+});
+router.post('/:id/changerole', checkRequiredFields(['role', 'utorid']), async (req, res) => {
+  sendResponse(res, await groupsModel.changeRole(parseInt(req.params.id), req.body.role, req.body.utorid, req.user));
+});
+router.post('/:id/invite', checkRequiredFields(['utorid']), async (req, res) => {
+  sendResponse(res, await groupsModel.invite(parseInt(req.params.id), req.body.utorid, req.user));
+});
+router.post('/:id/invite/accept', async (req, res) => {
+  sendResponse(res, await groupsModel.acceptInvite(parseInt(req.params.id), req.user));
+});
+router.post('/:id/invite/reject', async (req, res) => {
+  sendResponse(res, await groupsModel.rejectInvite(parseInt(req.params.id), req.user));
+});
 router.post('/:id/remove', routeNotImplemented);
 router.delete('/:id', routeNotImplemented);
 export default router;
