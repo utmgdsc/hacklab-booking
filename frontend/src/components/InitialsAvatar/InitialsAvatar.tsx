a8744039eab6d35e37f1c12e98de936f3c4d48f3
logonoff <logonoff@users.noreply.github.com>
2023-06-08 22:18:21 -0400

    initalsavatar.jsx -> tsx

--- frontend/src/components/InitialsAvatar/InitialsAvatar.tsx
+++ frontend/src/components/InitialsAvatar/InitialsAvatar.tsx
@@ -1,12 +1,19 @@
 import { Avatar } from "@mui/material";
 
+interface ColorHashProps {
+    /** the name to get the hash of */
+    name?: string;
+    /** the lightness of the color */
+    lightness?: number;
+}
+
 /**
  * get a hash of a string that is also a color code
  * @param {string} name the name to get the hash of
  * @param {number} lightness the lightness of the color
  * @returns {string} the color code in hsl format
  */
-const colorHash = ({ name = "", lightness = 50 }) => {
+const colorHash = ({ name = "", lightness = 50 }: ColorHashProps): string => {
     let hash = 95;
 
     // use djb2 algorithm to get a hash of the name
@@ -21,11 +28,17 @@ const colorHash = ({ name = "", lightness = 50 }) => {
     return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 };
 
+interface InitialsAvatarProps {
+    /** the name to get the initials of */
+    name?: string;
+}
+
 /**
  * Given a name get the initials of the name up to 2 characters into an avatar
  * @param {string} name the name to get the initials of
+ * @returns {JSX.Element} the avatar with the initials
  */
-export const InitialsAvatar = ({ name = "", ...props }) => {
+export const InitialsAvatar = ({ name = "", ...props }: InitialsAvatarProps): JSX.Element => {
     return (
         <Avatar alt={name} style={{ backgroundColor: colorHash({ name }), color: "white" }} {...props}>
             { name.match(/(^\S\S?|\s\S)?/g).map(v => v.trim()).join("").match(/(^\S|\S$)?/g).join("").toLocaleUpperCase()}
