84dc36ae27eefcf189bfc307040adcd84bad6242
Ido <benhaim.ido@gmail.com>
2023-06-10 00:54:40 -0400

    Fixed imports for dev env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,19 @@
 import common from './common';
+
 const { logger } = common;
 logger.info('App is starting..');
 logger.info('Connecting to database...');
 common.db.$connect().then(async () => {
   logger.info('Connected to database.');
   logger.info('Initializing api routes...');
+  if (process.env.NODE_ENV === 'development') {
+    await import('./types/express');
+  }
   await import('./api');
   logger.info('Initialized api.');
 
   logger.info('App started!');
-}).catch((e) => {
+}).catch((e: Error) => {
   logger.error('Failed to connect to database.');
   logger.error(e);
   process.exit(1);
