1f615147f9f8817ad4039a54acb09ac62be0a116
logonoff <logonoff@users.noreply.github.com>
2023-06-21 10:34:29 -0400

    fix prisma schema

--- reset_db_state.dev.ps1
+++ reset_db_state.dev.ps1
@@ -3,6 +3,8 @@ docker compose -f docker-compose.dev.yml down -v --rmi all --remove-orphans
 Remove-Item "./backend/logs" -Recurse
 Remove-Item "./db_data" -Recurse
 docker compose -f docker-compose.dev.yml up -d
-docker compose -f docker-compose.dev.yml exec backend bash -c "cd app;npx prisma migrate dev"
+
+timeout 30
 # restart backend to make sure it connects to the database
+docker compose -f docker-compose.dev.yml exec backend bash -c "cd app;npx prisma migrate dev"
 docker compose -f docker-compose.dev.yml restart backend
