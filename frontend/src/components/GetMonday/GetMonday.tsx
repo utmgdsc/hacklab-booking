fff224082ee9603eb5a965b09a53bce87f4cba0f
logonoff <logonoff@users.noreply.github.com>
2023-06-25 16:44:26 -0400

    Revert "refactor getmonday"
    This reverts commit e170d885bcc7200cf376372d89bd863a654cdbdc.

--- frontend/src/components/GetMonday/GetMonday.tsx
+++ frontend/src/components/GetMonday/GetMonday.tsx
@@ -1,5 +1,4 @@
-import dayjs, { OpUnitType } from "dayjs";
-import isoWeek from 'dayjs/plugin/isoWeek';
+import dayjs from "dayjs";
 
 /**
  * given any date, return the date of the Monday of that week.
@@ -9,9 +8,26 @@ import isoWeek from 'dayjs/plugin/isoWeek';
  * @param {Date} d the date to get the Monday of
  */
 export const GetMonday = (d: Date): Date => {
-  dayjs.extend(isoWeek);
+    let dayjsObj = dayjs(d);
+    const day = dayjsObj.day();
+    switch (day) {
+      case 0: // sunday - get the next monday
+        dayjsObj = dayjsObj.add(1, "day");
+        break;
+      case 1: // monday
+        break;
+      case 2:
+      case 3:
+      case 4:
+      case 5: // tuesday - friday: get current monday
+        dayjsObj = dayjsObj.subtract(day - 1, "day");
+        break;
+      case 6: // saturday - get the next monday
+        dayjsObj = dayjsObj.add(1, "day");
+        break;
+      default:
+        throw new Error("Invalid day");
+    }
 
-  let dayjsObj = dayjs(d).startOf("isoWeek" as OpUnitType);
-
-  return dayjsObj.toDate();
-};
+    return dayjsObj.toDate();
+  };
