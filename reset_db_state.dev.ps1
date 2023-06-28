4385c8d6b8184e69bd97f98001ece11eb674761b
logonoff <logonoff@users.noreply.github.com>
2023-06-28 13:38:48 -0400

    Update reset_db_state.dev.ps1

--- reset_db_state.dev.ps1
+++ reset_db_state.dev.ps1
@@ -1,3 +1,20 @@
+<#
+.SYNOPSIS
+    Resets the dev env. Only run this when needed.
+
+.DESCRIPTION
+    Resets the database state for the dev environment. This includes:
+    - removing all containers
+    - removing all volumes (including orphaned volumes)
+    - removing all images (including orphaned images)
+    - removing all logs
+    - removing all database data
+
+    After that, it starts the dev environment again and reconfigures postgres.
+
+    Precondition: Docker is running. The script will fail otherwise.
+#>
+
 # completely resets the database state
 docker compose -f docker-compose.dev.yml down -v --rmi all --remove-orphans
 Remove-Item "./backend/logs" -Recurse
