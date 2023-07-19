b856f310c41fe92dc9beedbc706804292a600218
Ido <benhaim.ido@gmail.com>
2023-07-19 00:59:22 -0400

    webhooks

--- backend/src/notifications/webhooks.ts
+++ backend/src/notifications/webhooks.ts
@@ -1,13 +1,16 @@
 import fetch from 'node-fetch';
 
-export const sendWebhook = async (url: string, data: any) => {
-  try {
-    await fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    });
-  } catch (e) {
-    console.log(e);
+export const sendWebhook = async (url: string, text: string) => {
+  if (url.includes('discord.com')) {
+    if (url.endsWith('/')) {
+      url = `${url}slack`;
+    } else {
+      url = `${url}/slack`;
+    }
   }
+  await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ text }),
+  });
 };
