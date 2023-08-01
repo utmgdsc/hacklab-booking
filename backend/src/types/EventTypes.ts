f3c00620d8a007378b249c2272ed435d0a8f5d79
Ido <benhaim.ido@gmail.com>
2023-07-31 21:13:41 -0400

    New events

--- backend/src/types/EventTypes.ts
+++ backend/src/types/EventTypes.ts
@@ -10,21 +10,35 @@ enum EventTypes {
   BOOKING_STATUS_CHANGED = 'BOOKING_STATUS_CHANGED',
   ADMIN_BOOKING_STATUS_CHANGED = 'ADMIN_BOOKING_STATUS_CHANGED',
   // context: full_name, room, room_friendly, utorid, approver_utorid, approver_full_name
+  ROOM_ACCESS_REQUESTED = 'ROOM_ACCESS_REQUESTED',
   ROOM_ACCESS_GRANTED = 'ROOM_ACCESS_GRANTED',
   ADMIN_ROOM_ACCESS_GRANTED = 'ADMIN_ROOM_ACCESS_GRANTED',
+  // context: full_name, room, room_friendly, utorid, capacity
+  ADMIN_ROOM_CREATED = 'ADMIN_ROOM_CREATED',
   // context: full_name, room, room_friendly, utorid, approver_utorid, approver_full_name
   ROOM_ACCESS_REVOKED = 'ROOM_ACCESS_REVOKED',
+  // context: full_name, room, room_friendly, utorid
   ADMIN_ROOM_ACCESS_REVOKED = 'ADMIN_ROOM_ACCESS_REVOKED',
-  // context: full_name, group_name, utorid, inviter_utorid, inviter_full_name
+  // context: full_name, group_name, group_id, utorid
+  ADMIN_GROUP_CREATED = 'ADMIN_GROUP_CREATED',
+  // context: full_name, group_name, group_id, group_id, utorid, inviter_utorid, inviter_full_name
   GROUP_MEMBER_INVITED = 'GROUP_MEMBER_INVITED',
-  // context: full_name, group_name, utorid, inviter_utorid, inviter_full_name
+  // context: full_name, group_name, group_id, utorid, inviter_utorid, inviter_full_name
   USER_INVITED_TO_GROUP = 'USER_INVITED_TO_GROUP',
-  // context: full_name, group_name, utorid, inviter_utorid, inviter_full_name
+  // context: full_name, group_name, group_id, utorid
   GROUP_MEMBER_JOINED = 'GROUP_MEMBER_JOINED',
-  // context: full_name, group_name, utorid, remover_utorid, remover_full_name
+  // context: full_name, group_name, group_id, utorid, remover_utorid, remover_full_name
   GROUP_MEMBER_REMOVED = 'GROUP_MEMBER_REMOVED',
-  // context: full_name, group_name, utorid
-  GROUP_MEMBER_LEFT = 'GROUP_MEMBER_LEFT',
+  // context: full_name, group_name, group_id, utorid, remover_utorid, remover_full_name
+  USER_REMOVED_FROM_GROUP = 'USER_REMOVED_FROM_GROUP',
+  // context: full_name, group_name, group_id, utorid, changer_utorid, changer_full_name, role
+  GROUP_ROLE_CHANGED = 'GROUP_ROLE_CHANGED',
+  USER_GROUP_ROLE_CHANGED = 'MANAGER_GROUP_ROLE_CHANGED',
+  // context: full_name, group_name, group_id, utorid
+  GROUP_DELETED = 'GROUP_DELETED',
+  // context: full_name, group_name, group_id, utorid
+  ADMIN_GROUP_DELETED = 'ADMIN_GROUP_DELETED',
+
 }
 
 export default EventTypes;
--- backend/src/types/NotificationContext.ts
+++ backend/src/types/NotificationContext.ts
@@ -1,3 +1,5 @@
+import { RequestStatus } from '@prisma/client';
+
 interface FullContext {
   receiver_utorid: string;
   receiver_full_name: string;
@@ -21,10 +23,68 @@ export interface BaseBookingContext extends NotificationPartialContext {
 }
 
 export type BookingCreatedContext = BaseBookingContext;
+
 export interface BookingStatusChangeContext extends BaseBookingContext {
   approver_utorid: string;
   approver_full_name: string;
-  reason: string;
+  status: RequestStatus;
+  reason?: string;
 }
-export type AllContexts = BookingCreatedContext | BookingStatusChangeContext;
+
+interface ActionContext extends NotificationPartialContext {
+  full_name: string;
+  utorid: string;
+}
+
+export interface RoomContext extends NotificationPartialContext {
+  room: string;
+  room_friendly: string;
+}
+
+export type RoomActionContext = RoomContext & ActionContext;
+export type RoomAccessRequestContext = RoomActionContext;
+
+export interface RoomAccessContext extends RoomActionContext {
+  approver_utorid: string;
+  approver_full_name: string;
+}
+
+export interface RoomCreatedContext extends RoomActionContext {
+  capacity: number;
+}
+
+export interface GroupContext extends NotificationPartialContext {
+  group_name: string;
+  group_id: string;
+}
+
+export type GroupActionContext = GroupContext & ActionContext;
+export type GroupCreatedContext = GroupActionContext;
+
+export interface GroupMemberInvitedContext extends GroupActionContext {
+  inviter_utorid: string;
+  inviter_full_name: string;
+}
+
+export interface GroupMemberRemovedContext extends GroupActionContext {
+  remover_utorid: string;
+  remover_full_name: string;
+}
+
+export interface GroupRoleChangedContext extends GroupActionContext {
+  changer_utorid: string;
+  changer_full_name: string;
+  role: string;
+}
+
+export type AllContexts =
+    BookingCreatedContext
+    | BookingStatusChangeContext
+    | RoomAccessRequestContext
+    | RoomAccessContext
+    | RoomCreatedContext
+    | GroupCreatedContext
+    | GroupMemberInvitedContext
+    | GroupMemberRemovedContext
+    | GroupRoleChangedContext;
 export type NotificationFullContext = FullContext & AllContexts;
