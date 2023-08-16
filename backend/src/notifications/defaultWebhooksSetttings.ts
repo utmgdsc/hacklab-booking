8c910947c1ddfe5bec1fa5a09aab83aa34224fff
Ido <benhaim.ido@gmail.com>
2023-08-16 12:37:31 -0400

    Update defaultWebhooksSetttings.ts

--- backend/src/notifications/defaultWebhooksSetttings.ts
+++ backend/src/notifications/defaultWebhooksSetttings.ts
@@ -3,15 +3,33 @@ import EventTypes from '../types/EventTypes';
 import { read } from 'fs';
 import logger from '../common/logger';
 
-const defaultWebhooksSetttings : UserWebhooks = {};
+const defaultWebhooksSetttings: UserWebhooks = {};
 
-defaultWebhooksSetttings[EventTypes.BOOKING_APPROVAL_REQUESTED] = [WebhookTypes.discord, WebhookTypes.email, WebhookTypes.slack];
-defaultWebhooksSetttings[EventTypes.BOOKING_STATUS_CHANGED] = [WebhookTypes.discord, WebhookTypes.email, WebhookTypes.slack];
-defaultWebhooksSetttings[EventTypes.ADMIN_BOOKING_UPDATED] = [WebhookTypes.discord, WebhookTypes.email, WebhookTypes.slack];
-defaultWebhooksSetttings[EventTypes.ADMIN_BOOKING_CREATED] = [WebhookTypes.discord, WebhookTypes.email, WebhookTypes.slack];
-defaultWebhooksSetttings[EventTypes.ADMIN_BOOKING_STATUS_CHANGED] = [WebhookTypes.discord, WebhookTypes.email, WebhookTypes.slack];
-defaultWebhooksSetttings[EventTypes.ADMIN_ROOM_ACCESS_REVOKED] = [WebhookTypes.discord, WebhookTypes.email, WebhookTypes.slack];
-defaultWebhooksSetttings[EventTypes.ADMIN_ROOM_ACCESS_GRANTED] = [WebhookTypes.discord, WebhookTypes.email, WebhookTypes.slack];
+defaultWebhooksSetttings[EventTypes.BOOKING_APPROVAL_REQUESTED] = [
+  WebhookTypes.discord,
+  WebhookTypes.email,
+  WebhookTypes.slack,
+];
+defaultWebhooksSetttings[EventTypes.BOOKING_STATUS_CHANGED] = [
+  WebhookTypes.discord,
+  WebhookTypes.email,
+  WebhookTypes.slack,
+];
+defaultWebhooksSetttings[EventTypes.ROOM_ACCESS_GRANTED] = [
+  WebhookTypes.discord,
+  WebhookTypes.email,
+  WebhookTypes.slack,
+];
+defaultWebhooksSetttings[EventTypes.ROOM_ACCESS_REVOKED] = [
+  WebhookTypes.discord,
+  WebhookTypes.email,
+  WebhookTypes.slack,
+];
+defaultWebhooksSetttings[EventTypes.ROOM_ACCESS_REQUESTED] = [
+  WebhookTypes.discord,
+  WebhookTypes.email,
+  WebhookTypes.slack,
+];
 
 logger.debug(`defaultWebhooksSetttings: ${JSON.stringify(defaultWebhooksSetttings)}`);
 export default defaultWebhooksSetttings as { readonly [key: string]: WebhookTypes[] };
