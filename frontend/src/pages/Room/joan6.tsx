18d819f588d03eb9474494159da0533288ee7d16
Ido <benhaim.ido@gmail.com>
2025-01-15 19:12:53 -0500

    Revert "Auto refresh"
    This reverts commit 17830928e5d4b7bfe03dc554c04cb2fb081d5867.

--- frontend/src/pages/Room/joan6.tsx
+++ frontend/src/pages/Room/joan6.tsx
@@ -240,15 +240,6 @@ export const Joan6 = () => {
         }, 1000);
         return () => clearInterval(interval);
     }, [update]);
-    useEffect(() => {
-        const interval = setInterval(
-            () => {
-                window.location.reload();
-            },
-            1000 * 60 * 60 * 24,
-        );
-        return () => clearInterval(interval);
-    });
 
     return (
         <Grid
